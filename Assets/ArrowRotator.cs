8782e7fe4d4e82cc04d25022f31d75856f04fea2
verymoko <verymoko@gmail.com>
2021-07-04 16:35:38 +0300

    Force Arrow

---
+++ Assets/ArrowRotator.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowRotator : MonoBehaviour
+{
+    public void OnClick()
+    {
+        transform.Rotate(0,0,180);
+    }
+    
+}
